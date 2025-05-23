5787a863ebe0aaacd5c29ec2ad24207f740883ab
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-05-23 19:31:45 +0300

    Apply Two way data binding

--- src/app/components/profile-card/profile-card.component.ts
+++ src/app/components/profile-card/profile-card.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-card',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.scss',
 })
