74241b030eecf379c24800d1f6ecfa0fdcff2d65
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-05-23 16:53:01 +0300

    Project Setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ProfileCardComponent } from './components/profile-card/profile-card.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'ProfileCard', component: ProfileCardComponent },
+  { path: '**', component: NotfoundComponent },
+];
